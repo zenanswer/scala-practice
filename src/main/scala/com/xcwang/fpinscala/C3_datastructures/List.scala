5757f84263dae28648e55a6e8d528e96963e90c5
xuechenwang <xuechenwang@cienet.com.cn>
2018-08-22 17:15:27 +0800

    Add more func in List and testing case

--- src/main/scala/com/xcwang/fpinscala/C3_datastructures/List.scala
+++ src/main/scala/com/xcwang/fpinscala/C3_datastructures/List.scala
@@ -3,6 +3,7 @@ package com.xcwang.fpinscala.C3_datastructures
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
+case class ConsLeft[+A](head: List[A], tail: A) extends List[A]
 
 object List{
   def apply[A](as: A*): List[A] = {
@@ -72,4 +73,98 @@ object List{
     case Cons(_, Nil) => Nil
     case Cons(head, tail) => Cons(head, init(tail))
   }
+
+
+  def foldRight[A, B](l: List[A], z:B)(f: (A, B)=>B):B = {
+    l match {
+      // z is a initial value
+      case Nil => z
+      case Cons(head, tail) => f(head, foldRight(tail, z)(f))
+      // foldRight(Cons(1,Cons(2,Cons(3,Cons(4,Nil)))), z)(f)
+      // ---------(-----------------------------------, -)(-)
+      // f(1, foldRight(Cons(2,Cons(3,Cons(4,Nil))), z)(f))
+      //      ---------(---------------------------, -)(-)
+      // f(1, f(2, foldRight(Cons(3,Cons(4,Nil)), z)(f)))
+      //           ---------(-------------------, -)(-)
+      // f(1, f(2, f(3, foldRight(Cons(4,Nil), z)(f))))
+      //                ---------(-----------, -)(-)
+      // f(1, f(2, f(3, f(4, foldRight(Nil, z)(f)))))
+      //                     ---------(---, -)(-)
+      // f(1, f(2, f(3, f(4, z))))
+    }
+  }
+
+  def length[A](as: List[A]): Int = {
+    foldRight(as, 0)((_:A, x:Int)=>x+1)
+  }
+
+  def foldLeft[A, B](l: List[A], z:B)(f: (B, A)=>B):B = {
+    l match {
+      case Nil => z
+      case Cons(head, tail) => foldLeft(tail, f(z,head))(f)
+    }
+    // foldLeft(Cons(1,Cons(2,Cons(3,Cons(4,Nil)))), z)(f)
+    // --------(-----------------------------------, -)(-)
+    // foldLeft(Cons(2,Cons(3,Cons(4,Nil))), f(z,1))(f)
+    // --------(---------------------------, ------)(-)
+    // foldLeft(Cons(3,Cons(4,Nil)), f(f(z,1),2))(f)
+    // --------(-------------------, -----------)(-)
+    // foldLeft(Cons(4,Nil), f(f(f(z,1),2),3))(f)
+    // --------(-----------, ----------------)(-)
+    // foldLeft(Nil, f(f(f(f(z,1),2),3),4))(f)
+    // --------(---, ---------------------)(-)
+    // f(f(f(f(z,1),2),3),4)
+  }
+
+  def reverse[A](l: List[A]): List[A] = {
+    foldLeft(l, Nil: List[A])((x:List[A], y:A) => Cons(y, x))
+  }
+
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h,t) => Cons(h, append(t, a2))
+    }
+//  TODO
+//  def appendLeft[A](l: List[A], n: A): List[A] = l match {
+//    case Nil => List(n)
+//    case Cons(head, tail) => foldLeft(tail, f(n, head, tail))
+//  }
+  def plus1(l: List[Int]): List[Int] = l match {
+    case Nil => Nil
+    case Cons(head, tail) => Cons(head+1, plus1(tail))
+  }
+
+  def map[A, B](l: List[A])(f: A=>B): List[B] = l match {
+    case Nil => Nil
+    case Cons(head, tail) => Cons(f(head), map(tail)(f))
+  }
+
+//  def filter[A](as: List[A])(f: A=>Boolean): List[A] = as match {
+//    case Nil => Nil
+//    case Cons(head, tail) => if(f(head)) Cons(head, filter(tail)(f)) else filter(tail)(f)
+//  }
+  def filter[A](as: List[A])(f: A=>Boolean): List[A] = {
+    foldRight(as, Nil: List[A])((h,t) => if (f(h)) Cons(h,t) else t)
+  }
+
+  def flatMap[A, B](as: List[A])(f: A=>List[B]): List[B] = as match {
+    case Nil => Nil
+    case Cons(head, tail) => append(f(head), flatMap(tail)(f))
+  }
+
+  def filter2[A](as: List[A])(f: A=>Boolean): List[A] = {
+    List.flatMap(as)(i=>if(f(i))List(i) else Nil)
+  }
+
+  def zipWith[A,B,C](l1: List[A], l2:List[B])(f: (A, B)=>C): List[C] = (l1, l2) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1,t1), Cons(h2,t2)) => Cons(f(h1,h2), zipWith(t1,t2)(f))
+  }
+
+  //TODO
+//  def hasSubSeq[A](l1: List[A], l2: List[A]): Boolean = {
+//
+//  }
 }
--- src/test/scala/com/xcwang/fpinscala/C3_datastructures/P3_ListTest.scala
+++ src/test/scala/com/xcwang/fpinscala/C3_datastructures/P3_ListTest.scala
@@ -132,4 +132,106 @@ class P3_ListTest extends WordSpec {
       }
     }
   }
+  "foldRight func" when {
+    "List(1,2,3,4) and A+B" should {
+      "10" in {
+        List.foldRight(List(1,2,3,4), 0)(_+_) shouldEqual 10
+      }
+    }
+    //P3_8
+    "List(1,2,3,4) and Cons(_+_)" should {
+      "List(1,2,3,4)" in {
+        List.foldRight(List(1,2,3,4), Nil: List[Int])(Cons(_, _)) shouldEqual List(1,2,3,4)
+      }
+    }
+    //P3_9
+    "List(1,2,3,4) 's length bases on foldRight" should {
+      "4" in {
+        List.length(List(1,2,3,4)) shouldEqual 4
+      }
+    }
+  }
+  //P3_10
+  "foldLeft func" when {
+    //P3_11
+    "List(1,2,3,4) and A+B" should {
+      "10" in {
+        List.foldLeft(List(1,2,3,4), 0)(_+_) shouldEqual 10
+      }
+    }
+    "List(1,2,3,4) and Cons(_+_)" should {
+      "not List(1,2,3,4) Cons, but ConsLeft" in {
+        List.foldLeft(List(1,2,3,4), Nil: List[Int])(ConsLeft(_, _)) shouldEqual ConsLeft(ConsLeft(ConsLeft(ConsLeft(Nil,1),2),3),4)
+      }
+    }
+  }
+  //P3_12
+  "reverse func" when {
+    "List(1,2)" should {
+      "List(2,1)" in {
+        List.reverse(List(1, 2)) shouldEqual List(2, 1)
+      }
+    }
+    "List(1,2,3,4)" should {
+      "List(4,3,2,1)" in {
+        List.reverse(List(1, 2, 3, 4)) shouldEqual List(4, 3, 2, 1)
+      }
+    }
+    "Nil" should {
+      "Nil" in {
+        List.reverse(Nil) shouldEqual Nil
+      }
+    }
+  }
+  //P3_14
+  "append func" when {
+    "List(1,2).append List[3,4]" should {
+      "List(1,2,3,4)" in {
+        List.append(List(1, 2), List(3,4)) shouldEqual List(1, 2, 3,4)
+      }
+    }
+  }
+  "P3_16 plus 1 func" when {
+    "List(1,2,3,4)" should {
+      "List(2,3,4,5)" in {
+        List.plus1(List(1, 2, 3, 4)) shouldEqual List(2, 3, 4, 5)
+      }
+    }
+  }
+  "P3_17/18 Double to String bases foreach func" when {
+    "List(1.0,2.0,3.0,4.0)" should {
+      "List(\"1.0\", \"2.0\", \"3.0\", \"4.0\")" in {
+        List.map(List(1.0,2.0,3.0,4.0))(_.toString) shouldEqual List("1.0", "2.0", "3.0", "4.0")
+      }
+    }
+  }
+  "P3_19 filter func" when {
+    "List(1,2,3,4,5,6)" should {
+      "List(2,4,6)" in {
+        List.filter(List(1,2,3,4,5,6))(_%2 == 0) shouldEqual List(2,4,6)
+      }
+    }
+  }
+  "P3_20 flatMap func" when {
+    "List(1,2,3,4)" should {
+      "List(1,1,2,2,3,3,4,4)" in {
+        List.flatMap(List(1,2,3,4))(i=>List(i,i)) shouldEqual List(1,1,2,2,3,3,4,4)
+      }
+    }
+  }
+  "P3_21 filter bases flatMap func" when {
+    "List(1,2,3,4,5,6)" should {
+      "List(2,4,6)" in {
+        //List.flatMap(List(1,2,3,4,5,6))(i=>if(i%2==0)List(i) else Nil) shouldEqual List(2,4,6)
+        List.filter2(List(1,2,3,4,5,6))(_%2 == 0) shouldEqual List(2,4,6)
+      }
+    }
+  }
+  "P3_22/23 zipWith func" when {
+    "List(1,2,3) List(4,5,6)" should {
+      "List(5,7,9)" in {
+        List.zipWith(List(1,2,3), List(4,5,6))(_+_) shouldEqual List(5,7,9)
+      }
+    }
+  }
 }
